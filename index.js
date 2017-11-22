45c28154da7b8e57b3b77e7188ff513b301697eb
Vlad Mashkin <jackonerichardson@gmail.com>
2017-11-22 13:44:25 +0200

    Add basic loader code.

---
+++ index.js
@@ -0,0 +1,21 @@
+import * as Mustache from 'mustache';
+import {getOptions} from 'loader-utils';
+
+export default function (source) {
+    const options = getOptions(this) || {};
+
+    let viewData = {};
+
+    if (options.viewData) {
+        viewData = options.viewData;
+    }
+    if (options.delimiters) {
+        if (!Array.isArray(options.delimiters) || options.delimiters.length !== 2) {
+            throw new Error('Custom delimiters option must be an array consisting of the opening and closing tag values.');
+        }
+
+        Mustache.tags = options.delimiters;
+    }
+
+    return Mustache.render(source, viewData);
+}
